eb41a5479a38a65ca2c2cf9e645f86c8861af881
Kanaka Meier <kanakameier@gmail.com>
2024-04-26 12:44:08 -0400

    Updated website and got a picture inserted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 // const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  content: [
+    "index.html",
+    "portfolio.html",
+    "contact.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue,html}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        whiteBeige: "edede9",
-        grayishColor: "d6ccc2",
-        lightBeige: "f5ebe0",
-        creamBeige: "e3d5ca",
-        darkBeige: "d5bdaf",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
